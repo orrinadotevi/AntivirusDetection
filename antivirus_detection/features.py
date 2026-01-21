83a10c0211f56ec35c25fd241527db660f740c80
Orrin Adotevi <oadotevi3@gatech.edu>
2026-01-20 23:27:55 -0500

    Patched features.py with ruff

--- antivirus_detection/features.py
+++ antivirus_detection/features.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import math
 from pathlib import Path
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, Iterable, List, Tuple
 
 import pefile
 
